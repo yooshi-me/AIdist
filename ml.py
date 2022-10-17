02bb16ccd57bda1d8cc2ced12ab01e521df878d9
aist_pc_TEL <t.yamamoto@aist.go.jp>
2022-10-17 10:32:26 +0900

    ml.pyの中身を関数化、コメント追加

--- ml.py
+++ ml.py
@@ -45,6 +45,75 @@ class Net(nn.Module):
 
         return x
 
+def make_figure():
+    acc_fig = plt.figure(figsize=(12,8))
+    plt.rcParams["font.size"] = 25
+    acc_ax = acc_fig.add_subplot(111,xlabel="Epoch",ylabel="Accuracy")
+    acc_ax.set_ylim(0,100)
+
+    loss_fig = plt.figure(figsize=(12,8))
+    plt.rcParams["font.size"] = 25
+    loss_ax = loss_fig.add_subplot(111,xlabel="Epoch",ylabel="Loss")
+
+    return acc_fig,loss_fig,acc_ax,loss_ax
+
+def train_loop(trainloader,optimizer,loss_fn,net,device,data_for_glaph):
+    running_loss = 0
+    all_loss = 0
+    train_size = len(trainloader.dataset)
+    for i, (inputs,labels) in enumerate(trainloader):
+        correct = 0
+        optimizer.zero_grad()
+        inputs,labels = inputs.to(device),labels.to(device)
+
+        outputs = net(inputs)
+        outputs = torch.flatten(outputs)
+        labels = labels.to(torch.float32)
+        loss = loss_fn(outputs,labels)
+
+        running_loss += loss.item()
+        all_loss += loss.item()
+        predicted = torch.where(outputs<0.5,0,1)
+        correct += (predicted==labels).sum().item()
+        loss.backward()
+        optimizer.step()
+
+        if i % 100 == 99: 
+            running_loss /= 100
+            print(f"loss:{running_loss:>5f} [{i*len(inputs)}]/[{train_size}]")
+            running_loss = 0
+    
+    all_loss /= len(trainloader)
+    correct /= inputs.size(0)
+    correct *= 100
+    
+    data_for_glaph["train"].append([correct,all_loss])
+    print(f"Accuracy: {correct:>5f}% loss:{all_loss:>5f}")
+
+    return data_for_glaph
+
+def test_loop(testloader,loss_fn,net,device,data_for_glaph):
+    test_size = len(testloader.dataset)
+    all_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for inputs,labels in testloader:
+            inputs,labels = inputs.to(device),labels.to(device)
+
+            outputs = net(inputs)
+            outputs = torch.flatten(outputs)
+            labels = labels.to(torch.float32)
+            loss = loss_fn(outputs,labels)
+            all_loss += loss.item()
+            predicted = torch.where(outputs<0.5,0,1)
+            correct += (predicted==labels).sum().item()
+        all_loss /= len(testloader)
+        correct /= test_size
+        correct *= 100
+        data_for_glaph["test"].append([correct,all_loss])
+        print(f"Accuracy: {correct:>5f}% loss:{all_loss:>5f}")
+    
+    return data_for_glaph
 
 def google_drive_accese():
 
@@ -55,99 +124,44 @@ def google_drive_accese():
     drive = GoogleDrive(gauth)
 
 def main():
+    # ハイパーパラメータ
     epochs = 10
     batch_size = 50
     lr = 1e-3
 
     # 出力用ディレクトリ準備
-    paths_list_path = "./data/pahts_list.csv"
+    paths_list_path = "./data/pahts_list_mini.csv"
     out_path = "./out"
     out_path = os.path.join(out_path,datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S"))
 
-
-
     #google_drive_accese()
+
+    # データローダー取得
     trainloader,testloader = get_loader(paths_list_path,batch_size)
 
     # グラフ準備
-    acc_fig = plt.figure(figsize=(12,8))
-    plt.rcParams["font.size"] = 25
-    acc_ax = acc_fig.add_subplot(111,xlabel="Epoch",ylabel="Accuracy")
-    acc_ax.set_ylim(0,100)
-
-    loss_fig = plt.figure(figsize=(12,8))
-    plt.rcParams["font.size"] = 25
-    loss_ax = loss_fig.add_subplot(111,xlabel="Epoch",ylabel="Loss")
+    acc_fig, loss_fig, acc_ax, loss_ax = make_figure()
 
 
     # モデル準備   
     device = "cuda" if torch.cuda.is_available() else "cpu"
-    print("Device is ",device)
+    print("Device is",device)
     net = Net()
     net = net.to(device)
 
-    optimizer = optim.Adam(net.parameters(),lr=0.001)
+    optimizer = optim.Adam(net.parameters(),lr=lr)
     loss_fn = nn.BCELoss()
     data_for_glaph = {"train":[],"test":[]}
     
+    #学習、テスト
     try:
         for epoch in range(epochs):
-            running_loss = 0
-            all_loss = 0
-            train_size = len(trainloader.dataset)
             print(f"epoch:{epoch+1:>d} ------------------------")
-            for i, (inputs,labels) in enumerate(trainloader):
-                correct = 0
-                optimizer.zero_grad()
-                inputs,labels = inputs.to(device),labels.to(device)
+            data_for_glaph = train_loop(trainloader,optimizer,loss_fn,net,device,data_for_glaph)
+            data_for_glaph = test_loop(testloader,loss_fn,net,device,data_for_glaph)
 
-                outputs = net(inputs)
-                outputs = torch.flatten(outputs)
-                labels = labels.to(torch.float32)
-                loss = loss_fn(outputs,labels)
-
-                running_loss += loss.item()
-                all_loss += loss.item()
-                predicted = torch.where(outputs<0.5,0,1)
-                correct += (predicted==labels).sum().item()
-                loss.backward()
-                optimizer.step()
-
-                if i % 100 == 99: 
-                    running_loss /= 100
-                    print(f"loss:{running_loss:>5f} [{i*len(inputs)}]/[{train_size}]")
-                    running_loss = 0
-            
-            all_loss /= len(trainloader)
-            correct /= inputs.size(0)
-            correct *= 100
-            
-            data_for_glaph["train"].append([correct,all_loss])
-            print(f"Accuracy: {correct:>5f}% loss:{all_loss:>5f}")
-
-            test_size = len(testloader.dataset)
-            all_loss = 0
-            correct = 0
-            with torch.no_grad():
-                for inputs,labels in testloader:
-                    inputs,labels = inputs.to(device),labels.to(device)
-
-                    outputs = net(inputs)
-                    outputs = torch.flatten(outputs)
-                    labels = labels.to(torch.float32)
-                    loss = loss_fn(outputs,labels)
-                    all_loss += loss.item()
-                    predicted = torch.where(outputs<0.5,0,1)
-                    correct += (predicted==labels).sum().item()
-                all_loss /= len(testloader)
-                correct /= test_size
-                correct *= 100
-                data_for_glaph["test"].append([correct,all_loss])
-                print(f"Accuracy: {correct:>5f}% loss:{all_loss:>5f}")
-    
     except KeyboardInterrupt:
         pass
-    
     
     # グラフプロット
     if len(data_for_glaph["test"]) > 1:
@@ -163,9 +177,6 @@ def main():
     acc_fig.savefig(os.path.join(out_path,"accuracy_figure"))
     loss_fig.legend(bbox_to_anchor=(1, 1), loc='upper right', borderaxespad=0)    
     loss_fig.savefig(os.path.join(out_path,"loss_figure"))
-
-
-    
 
 
 if __name__ == "__main__":
